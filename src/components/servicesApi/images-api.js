e31f8e075e6e87a8c5fab006f71ffe62ee89cb09
Aleksey-7 <84351678+Aleksey-7@users.noreply.github.com>
2023-03-05 16:38:56 +0200

    create image-finder
    create image-finder

---
+++ src/components/servicesApi/images-api.js
@@ -0,0 +1,10 @@
+const URL = 'https://pixabay.com/api/';
+const KEY = '31882086-c73dad84d82fd65a5a1109d75';
+
+export default function fetchAPI(query, page = 1) {
+  return fetch(
+    `${URL}?q=${query}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
+  ).then(response => {
+    return response.json();
+  });
+}
